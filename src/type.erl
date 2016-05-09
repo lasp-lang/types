d017c0ffcf063e94a026f6e9cfca355f2dee87cd
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-05-09 14:32:19 +0100

    Prototype interface.
    Prototype general interface for all data types that support all
    operations needed for manipulation in Lasp and outside of Lasp.

---
+++ src/type.erl
@@ -0,0 +1,68 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2015-2016 Christopher Meiklejohn.  All Rights Reserved.
+%% Copyright (c) 2007-2012 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc Inspired by both the Lasp data type behaviour and the Riak data
+%%      type behaviour.
+
+-module(type).
+-author("Christopher Meiklejohn <christopher.meiklejohn@gmail.com>").
+
+%% Define some initial types.
+-type crdt() :: term().
+-type operation() :: term().
+-type actor() :: term().
+-type value() :: term().
+-type error() :: term().
+-type context() :: riak_dt_vclock:vclock() | undefined.
+-type threshold() :: value() | {strict, value()}.
+
+%% Initialize a CRDT.
+-callback new() -> crdt().
+
+%% Unified interface for allowing parameterized CRDTs.
+-callback new([term()]) -> term().
+
+%% Perform a mutation.
+-callback mutate(operation(), actor(), crdt()) ->
+    {ok, crdt()} | {error, error()}.
+-callback mutate(operation(), actor(), crdt(), context()) ->
+    {ok, crdt()} | {error, error()}.
+
+%% Perform a delta mutation.
+-callback delta_mutate(term(), actor(), crdt()) ->
+    {ok, crdt()} | {error, atom()}.
+
+%% Get the value of a CRDT.
+-callback query(crdt()) -> value().
+-callback query(term(), crdt()) -> value().
+
+%% Merge two replicas.
+-callback merge(crdt(), crdt()) -> crdt().
+
+%% Compare equality.
+-callback equal(crdt(), crdt()) -> boolean().
+
+%% Threshold testing.
+-callback threshold_met(crdt(), crdt(), threshold()) -> boolean().
+
+%% Inflation testing.
+-callback is_inflation(crdt(), crdt(), threshold()) -> boolean().
+-callback is_strict_inflation(crdt(), crdt(), threshold()) -> boolean().
