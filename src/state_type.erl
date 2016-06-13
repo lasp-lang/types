33e2437104dac9bc4368c1809194c3acb74c65c2
Junghun Yoo <junghun.yoo@cs.ox.ac.uk>
2016-06-13 13:55:03 +0100

    Add the first optimised version of the state_oorset_ps type

--- src/state_type.erl
+++ src/state_type.erl
@@ -39,7 +39,8 @@
                       state_pncounter |
                       state_twopset |
                       state_oorset |
-                      state_oorset_ps.
+                      state_oorset_ps |
+                      state_oorset_ps_v2.
 -type crdt() :: {state_type(), type:payload()}.
 -type delta_crdt() :: {state_type(), {delta, type:payload()}}.
 -type delta_or_state() :: crdt() | delta_crdt().
