39d5a43c36fb020618ff531df26dd9fb7f9e71b6
Georges Younes <georges.r.younes@gmail.com>
2016-07-07 14:28:19 +0100

    fix lint error

--- src/pure_ormap.erl
+++ src/pure_ormap.erl
@@ -18,13 +18,13 @@
 %%
 %% -------------------------------------------------------------------
 
-%% @doc Pure AWORSet CRDT: pure op-based add-wins observed-remove set
+%% @doc Pure ORMap CRDT: pure op-based observed-remove Map
 %%
 %% @reference Carlos Baquero, Paulo Sérgio Almeida, and Ali Shoker
 %%      Making Operation-based CRDTs Operation-based (2014)
 %%      [http://haslab.uminho.pt/ashoker/files/opbaseddais14.pdf]
 
--module(pure_aworset).
+-module(pure_ormap).
 -author("Georges Younes <georges.r.younes@gmail.com>").
 
 -behaviour(type).
@@ -40,26 +40,25 @@
 -export([mutate/3, query/1, equal/2]).
 -export([redundant/2, remove_redundant_crystal/2, remove_redundant_polog/2, check_stability/2]).
 
--export_type([pure_aworset/0, pure_aworset_op/0]).
+-export_type([pure_ormap/0, pure_ormap_op/0]).
 
--opaque pure_aworset() :: {?TYPE, payload()}.
+-opaque pure_ormap() :: {?TYPE, payload()}.
 -type payload() :: {pure_type:polog(), ordsets:set()}.
--type pure_aworset_op() :: {add, pure_type:element()} | {rmv, pure_type:element()}.
+-type pure_ormap_op() :: {add, pure_type:element()} | {rmv, pure_type:element()}.
 
-%% @doc Create a new, empty `pure_aworset()'
--spec new() -> pure_aworset().
+%% @doc Create a new, empty `pure_ormap()'
+-spec new() -> pure_ormap().
 new() ->
     {?TYPE, {orddict:new(), ordsets:new()}}.
 
-%% @doc Create a new, empty `pure_aworset()'
--spec new([term()]) -> pure_aworset().
+%% @doc Create a new, empty `pure_ormap()'
+-spec new([term()]) -> pure_ormap().
 new([]) ->
     new().
 
-%% @doc Check redundancy `pure_aworset()'
-%% For a given element, either an "add" or a "rmv" in the future of an "add" make it redundant.
+%% @doc Check redundancy `pure_ormap()'
 %% Called in remove_redundant().
--spec redundant({pure_type:id(), pure_aworset_op()}, {pure_type:id(), pure_aworset_op()}) ->
+-spec redundant({pure_type:id(), pure_ormap_op()}, {pure_type:id(), pure_ormap_op()}) ->
     integer().
 redundant({VV1, {add, Elem1}}, {VV2, {_X, Elem2}}) ->
     case Elem1 =:= Elem2 andalso pure_trcb:happened_before(VV1, VV2) of
@@ -69,9 +68,9 @@ redundant({VV1, {add, Elem1}}, {VV2, {_X, Elem2}}) ->
             0
     end.
 
-%% @doc Removes redundant operations from POLog of `pure_aworset()'
-%% Called upon updating (add, rmv) the `pure_aworset()'
--spec remove_redundant_polog({pure_type:id(), pure_aworset_op()}, pure_aworset()) -> {boolean(), pure_aworset()}.
+%% @doc Removes redundant operations from POLog of `pure_ormap()'
+%% Called upon updating (add, rmv) the `pure_ormap()'
+-spec remove_redundant_polog({pure_type:id(), pure_ormap_op()}, pure_ormap()) -> {boolean(), pure_ormap()}.
 remove_redundant_polog({VV1, Op}, {?TYPE, {POLog0, ORSet}}) ->
     POLog1 = orddict:fold(
         fun(Key, Value, Acc) ->
@@ -87,22 +86,21 @@ remove_redundant_polog({VV1, Op}, {?TYPE, {POLog0, ORSet}}) ->
     ),
     {true, {?TYPE, {POLog1, ORSet}}}.
 
-%% @doc Removes redundant operations from Crystal of `pure_aworset()'
-%% If there is an "add" or "rmv" for some element, that element does not need to be in the sequential set.
-%% Called upon updating (add, rmv) the `pure_aworset()'
--spec remove_redundant_crystal({pure_type:id(), pure_aworset_op()}, pure_aworset()) -> {boolean(), pure_aworset()}.
-remove_redundant_crystal({_VV1, {_X, Elem}}, {?TYPE, {POLog, AWORSet}}) ->
-    case ordsets:is_element(Elem, AWORSet) of
+%% @doc Removes redundant operations from POLog of `pure_ormap()'
+%% Called upon updating (add, rmv) the `pure_ormap()'
+-spec remove_redundant_crystal({pure_type:id(), pure_ormap_op()}, pure_ormap()) -> {boolean(), pure_ormap()}.
+remove_redundant_crystal({_VV1, {_X, Elem}}, {?TYPE, {POLog, ORMap}}) ->
+    case ordsets:is_element(Elem, ORMap) of
         true ->
-            {true, {?TYPE, {POLog, ordsets:del_element(Elem, AWORSet)}}};
+            {true, {?TYPE, {POLog, ordsets:del_element(Elem, ORMap)}}};
         false ->
-            {true, {?TYPE, {POLog, AWORSet}}}
+            {true, {?TYPE, {POLog, ORMap}}}
     end.
 
-%% @doc Checks stable operations and remove them from POLog of `pure_aworset()'
--spec check_stability(pure_type:id(), pure_aworset()) -> pure_aworset().
-check_stability(StableVV, {?TYPE, {POLog0, AWORSet0}}) ->
-    {POLog1, AWORSet1} = orddict:fold(
+%% @doc Checks stable operations and remove them from POLog of `pure_ormap()'
+-spec check_stability(pure_type:id(), pure_ormap()) -> pure_ormap().
+check_stability(StableVV, {?TYPE, {POLog0, ORMap0}}) ->
+    {POLog1, ORMap1} = orddict:fold(
         fun(Key, {_Op, Elem}=Value, {AccPOLog, AccORSet}) ->
             case pure_trcb:happened_before(Key, StableVV) of
                 true ->
@@ -111,35 +109,35 @@ check_stability(StableVV, {?TYPE, {POLog0, AWORSet0}}) ->
                     {orddict:store(Key, Value, AccPOLog), AccORSet}
             end
         end,
-        {orddict:new(), AWORSet0},
+        {orddict:new(), ORMap0},
         POLog0
     ),
-    {?TYPE, {POLog1, AWORSet1}}.
+    {?TYPE, {POLog1, ORMap1}}.
 
-%% @doc Update a `pure_aworset()'.
--spec mutate(pure_aworset_op(), pure_type:id(), pure_aworset()) ->
-    {ok, pure_aworset()}.
-mutate({add, Elem}, VV, {?TYPE, {POLog, PureAWORSet}}) ->
-    {_, {?TYPE, {POLog0, PureAWORSet0}}} = pure_polog:remove_redundant({VV, {add, Elem}}, {?TYPE, {POLog, PureAWORSet}}),
-    {ok, {?TYPE, {orddict:store(VV, {add, Elem}, POLog0), PureAWORSet0}}};
-mutate({rmv, Elem}, VV, {?TYPE, {POLog, PureAWORSet}}) ->
-    {_, {?TYPE, {POLog0, PureAWORSet0}}} = pure_polog:remove_redundant({VV, {rmv, Elem}}, {?TYPE, {POLog, PureAWORSet}}),
-    {ok, {?TYPE, {POLog0, PureAWORSet0}}}.
+%% @doc Update a `pure_ormap()'.
+-spec mutate(pure_ormap_op(), pure_type:id(), pure_ormap()) ->
+    {ok, pure_ormap()}.
+mutate({add, Elem}, VV, {?TYPE, {POLog, PureORMap}}) ->
+    {_, {?TYPE, {POLog0, PureORMap0}}} = pure_polog:remove_redundant({VV, {add, Elem}}, {?TYPE, {POLog, PureORMap}}),
+    {ok, {?TYPE, {orddict:store(VV, {add, Elem}, POLog0), PureORMap0}}};
+mutate({rmv, Elem}, VV, {?TYPE, {POLog, PureORMap}}) ->
+    {_, {?TYPE, {POLog0, PureORMap0}}} = pure_polog:remove_redundant({VV, {rmv, Elem}}, {?TYPE, {POLog, PureORMap}}),
+    {ok, {?TYPE, {POLog0, PureORMap0}}}.
 
-%% @doc Returns the value of the `pure_aworset()'.
-%%      This value is a set with all the elements in the `pure_aworset()'.
--spec query(pure_aworset()) -> sets:set(pure_type:element()).
-query({?TYPE, {POLog0, PureAWORSet0}}) ->
-    Elements0 = ordsets:to_list(PureAWORSet0),
+%% @doc Returns the value of the `pure_ormap()'.
+%%      This value is a set with all the elements in the `pure_ormap()'.
+-spec query(pure_ormap()) -> sets:set(pure_type:element()).
+query({?TYPE, {POLog0, PureORMap0}}) ->
+    Elements0 = ordsets:to_list(PureORMap0),
     Elements1 = [El || {_Key, {_Op, El}} <- orddict:to_list(POLog0)],
     sets:from_list(lists:append(Elements0, Elements1)).
 
 
-%% @doc Equality for `pure_aworset()'.
--spec equal(pure_aworset(), pure_aworset()) -> boolean().
-equal({?TYPE, {POLog1, PureAWORSet1}}, {?TYPE, {POLog2, PureAWORSet2}}) ->
+%% @doc Equality for `pure_ormap()'.
+-spec equal(pure_ormap(), pure_ormap()) -> boolean().
+equal({?TYPE, {POLog1, PureORMap1}}, {?TYPE, {POLog2, PureORMap2}}) ->
     Fun = fun(Value1, Value2) -> Value1 == Value2 end,
-    ordsets_ext:equal(PureAWORSet1, PureAWORSet2) andalso orddict_ext:equal(POLog1, POLog2, Fun).
+    ordsets_ext:equal(PureORMap1, PureORMap2) andalso orddict_ext:equal(POLog1, POLog2, Fun).
 
 
 %% ===================================================================
@@ -156,15 +154,15 @@ redundant_test() ->
     ?assertEqual(0, redundant({[{0, 0}, {1, 0}], {add, <<"a">>}}, {[{0, 0}, {1, 0}], {add, <<"a">>}})).
 
 remove_redundant_crystal_test() ->
-    {Redundant0, {?TYPE, {_POLog0, AWORSet0}}} = remove_redundant_crystal({[{0, 1}, {1, 2}, {2, 3}], {add, <<"a">>}}, {?TYPE, {[{0, 1}], [<<"a">>, <<"b">>, <<"c">>]}}),
+    {Redundant0, {?TYPE, {_POLog0, ORMap0}}} = remove_redundant_crystal({[{0, 1}, {1, 2}, {2, 3}], {add, <<"a">>}}, {?TYPE, {[{0, 1}], [<<"a">>, <<"b">>, <<"c">>]}}),
     ?assertEqual(true, Redundant0),
-    ?assertEqual([<<"b">>, <<"c">>], AWORSet0),
-    {Redundant1, {?TYPE, {_POLog1, AWORSet1}}} = remove_redundant_crystal({[{0, 1}, {1, 2}, {2, 3}], {rmv, <<"a">>}}, {?TYPE, {[{0, 1}], [<<"a">>, <<"b">>, <<"c">>]}}),
+    ?assertEqual([<<"b">>, <<"c">>], ORMap0),
+    {Redundant1, {?TYPE, {_POLog1, ORMap1}}} = remove_redundant_crystal({[{0, 1}, {1, 2}, {2, 3}], {rmv, <<"a">>}}, {?TYPE, {[{0, 1}], [<<"a">>, <<"b">>, <<"c">>]}}),
     ?assertEqual(true, Redundant1),
-    ?assertEqual([<<"b">>, <<"c">>], AWORSet1),
-    {Redundant2, {?TYPE, {_POLog2, AWORSet2}}} = remove_redundant_crystal({[{0, 1}], {rmv, <<"d">>}}, {?TYPE, {[{0, 1}], [<<"a">>]}}),
+    ?assertEqual([<<"b">>, <<"c">>], ORMap1),
+    {Redundant2, {?TYPE, {_POLog2, ORMap2}}} = remove_redundant_crystal({[{0, 1}], {rmv, <<"d">>}}, {?TYPE, {[{0, 1}], [<<"a">>]}}),
     ?assertEqual(true, Redundant2),
-    ?assertEqual([<<"a">>], AWORSet2).
+    ?assertEqual([<<"a">>], ORMap2).
 
 query_test() ->
     Set0 = new(),
