1d2993a4d8cbfca1cad38c657095e1cfc81e9565
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-07-15 11:09:02 +0100

    Start with composability tests

---
+++ test/state_type_composability_SUITE.erl
@@ -0,0 +1,72 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2016 Christopher Meiklejohn.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+%%
+
+-module(state_type_composability_SUITE).
+-author("Vitor Enes Duarte <vitorenesduarte@gmail.com>").
+
+%% common_test callbacks
+-export([init_per_suite/1,
+         end_per_suite/1,
+         init_per_testcase/2,
+         end_per_testcase/2,
+         all/0]).
+
+%% tests
+-compile([export_all]).
+
+-include("state_type.hrl").
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+%% ===================================================================
+%% common_test callbacks
+%% ===================================================================
+
+init_per_suite(Config) -> Config.
+end_per_suite(Config) -> Config.
+init_per_testcase(_Case, Config) -> Config.
+end_per_testcase(_Case, Config) -> Config.
+
+all() ->
+		[
+		 pair_with_gcounters_test
+		].
+
+%% ===================================================================
+%% tests
+%% ===================================================================
+
+pair_with_gcounters_test(_Config) ->
+    Actor = 1,
+    Pair0 = ?PAIR_TYPE:new([?GCOUNTER_TYPE, ?GCOUNTER_TYPE]),
+    {ok, Pair1} = ?PAIR_TYPE:mutate({fst, increment}, Actor, Pair0),
+    {ok, Pair2} = ?PAIR_TYPE:mutate({snd, increment}, Actor, Pair1),
+    {ok, Pair3} = ?PAIR_TYPE:mutate({fst, increment}, Actor, Pair2),
+    Query = ?PAIR_TYPE:query(Pair3),
+    ?assertEqual({?PAIR_TYPE, {{?GCOUNTER_TYPE, []}, {?GCOUNTER_TYPE, []}}}, Pair0),
+    ?assertEqual({?PAIR_TYPE, {{?GCOUNTER_TYPE, [{1, 1}]}, {?GCOUNTER_TYPE, []}}}, Pair1),
+    ?assertEqual({?PAIR_TYPE, {{?GCOUNTER_TYPE, [{1, 1}]}, {?GCOUNTER_TYPE, [{1, 1}]}}}, Pair2),
+    ?assertEqual({?PAIR_TYPE, {{?GCOUNTER_TYPE, [{1, 2}]}, {?GCOUNTER_TYPE, [{1, 1}]}}}, Pair3),
+    ?assertEqual({2, 1}, Query),
+    ok.
+
+	
