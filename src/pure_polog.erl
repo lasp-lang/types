9dd3298606a02d9b7c7c930f4caacecfb143fce6
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-08-15 16:03:36 -0700

    Remove incorrect copyright.

--- src/pure_polog.erl
+++ src/pure_polog.erl
@@ -1,7 +1,5 @@
-%% -------------------------------------------------------------------
 %%
 %% Copyright (c) 2015-2016 Christopher Meiklejohn.  All Rights Reserved.
-%% Copyright (c) 2007-2012 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
